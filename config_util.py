ca3edc00a06e8e2b7509ea971257b2cd26722baf
HENDRIX-ZT2 <arnfinn@spholderer.de>
2019-05-26 22:09:05 +0200

    Fix line end format

--- config_util.py
+++ config_util.py
@@ -1,13 +1,14 @@
 
 def read_config(cfg_path):
-	with open(cfg_path, 'rb') as cfg_file:
+	with open(cfg_path, 'r', encoding='utf-8') as cfg_file:
 		cfg = {}
-		for line in cfg_file.read().decode("utf-8").split("\r\n"):
-			if line.strip() and not line.startswith("#") and "=" in line:
-				(key, val) = line.split("=")
+		for line in cfg_file:
+			if not line.startswith("#") and "=" in line:
+				(key, val) = line.strip().split("=")
 				key = key.strip()
 				val = val.strip()
 				if val.startswith("["):
+					#strip list format [' ... ']
 					val = val[2:-2]
 					cfg[key] = [v.strip() for v in val.split("', '")]					
 				else:
@@ -15,9 +16,9 @@ def read_config(cfg_path):
 		return cfg
 
 def write_config(cfg_path, cfg):
-	stream = "\r\n".join( [key+"="+str(val) for key, val in cfg.items()] )
-	with open(cfg_path, 'wb') as cfg_file:
-		cfg_file.write(stream.encode("utf-8"))
+	stream = "\n".join( [key+"="+str(val) for key, val in cfg.items()] )
+	with open(cfg_path, 'w', encoding='utf8') as cfg_file:
+		cfg_file.write(stream)
 
 if __name__ == '__main__':
 	cfg = read_config("config.ini")
